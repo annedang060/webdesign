5a0b78bfde2d0b55aee2e9ab0d41ea3e51b04a81
annedang060 <43803827+annedang060@users.noreply.github.com>
2020-02-16 00:44:33 -0800

    Add files via upload

---
+++ calculator/index.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+ <link href="style.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<body>
+<div class="main">
+<form>
+    <input type="text" name="num1" placeholder="Number1">
+    <input type="text" name="num2" placeholder="Number2">
+    <select name="operator">
+        <option>None</option>
+        <option>Add</option>
+        <option>Subtract</option>
+        <option>Multiply</option>
+        <option>Divide</option>
+    </select>
+    <br>
+    <button type="submit" name="submit" value="submit">Calculate</button>
+</form>
+
+
+    <div class="answer">
+<?php
+  if(isset($_GET['submit'])){
+      $result1 = $_GET['num1'];
+      $result2 = $_GET['num2'];
+      $operator = $_GET['operator'];
+      switch( $operator){
+          case "None":
+              echo "you need to select a method!";
+              break;
+          case "Add":
+              echo $result1 + $result2;
+              break;
+          case "Subtract":
+              echo $result1 - $result2;
+              break;
+          case "Multiply":
+              echo $result1 * $result2;
+              break;
+          case "Divide":
+              echo $result1 / $result2;
+              break;
+      }
+  }
+
+
+?>
+    </div>
+</div>
+
+</body>
+
+</html>
